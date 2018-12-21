182c9d9fb0a02251c5085d65e8038324c6d26f7a
yoonki.kim <yoonki.kim71@gmail.com>
2018-12-21 17:35:00 +0900

    test and study micropython

--- Codings/ESP32/MicroPython/NodeMCU_ESP-32S_Project/test.py
+++ Codings/ESP32/MicroPython/NodeMCU_ESP-32S_Project/test.py
@@ -1,19 +1,51 @@
-class Person:
-    count = 0
-    def __init__(self):
-        Person.count += 1
+
+def add_metaclass(metaclass):
+    """Class decorator for creating a class with a metaclass."""
+
+    def wrapper(cls):
+        orig_vars = cls.__dict__.copy()
+        orig_vars.pop('__dict__', None)
+        orig_vars.pop('__weakref__', None)
+        slots = orig_vars.get('__slots__')
+        if slots is not None:
+            if isinstance(slots, str):
+                slots = [slots]
+            for slots_var in slots:
+                orig_vars.pop(slots_var)
+        return metaclass(cls.__name__, cls.__bases__, orig_vars)
+    return wrapper
+
+
+class AddressTracker(type):
+    """Metaclass keeping track of all the types of Address we understand."""
+
+    def __init__(cls, name, bases, attrs):
+        if not hasattr(cls, '_addrtypes'):
+            cls._addrtypes = []
+        else:
+            cls._addrtypes.append(cls)
+
+
+@add_metaclass(AddressTracker)
+class Address(object):
+    """An address for one or more ballasts."""
 
     @classmethod
-    def print_count(cls):
-        print('{0} created.'.format(cls.count))
+    def from_frame(cls, f):
+        """Return an Address object
 
-    #@staticmethod
-    def print_count1():
-        print('{0} created.'.format(Person.count))
+        If the frame has a destination address, return a suitable
+        Address object; otherwise return None.
+        """
+        if cls != Address:
+            return
+        for at in cls._addrtypes:
+            r = at.from_frame(f)
+            if r:
+                return r
 
-james = Person()
-maria = Person()
-richard = Person()
+    def add_to_frame(self, f):
+        raise IncompatibleFrame("Cannot add unknown address to any frame")
 
-Person.print_count()
-Person.print_count1()
+    def __str__(self):
+        return "<no address>"
